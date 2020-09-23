b429c80f1f0077d12e2972fd441770e1a7a36342
zhangy04 <zhangy04@mingyuanyun.com>
2020-09-23 11:38:05 +0800

    EChart

---
+++ GlassSmile/src/ECharts/echarts.custom.js
@@ -0,0 +1,6 @@
+export * from 'echarts/src/echarts';
+// 引入饼图。
+import 'echarts/src/chart/pie';
+import 'echarts/src/chart/bar';
+import 'echarts/src/component/title';
+import 'echarts/src/component/tooltip';
